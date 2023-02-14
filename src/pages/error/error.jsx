6103c13539decd66075040dc14f0ce8f43b8212b
alexperronnet <github.ayfb9@lxprnt.com>
2023-02-14 19:05:16 +0100

    :construction: Create optional pages

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,3 +1,22 @@
+import { Link } from 'react-router-dom'
 import css from '@/pages/error/error.module.scss'
 
-export const Error = () => <section className={css.error}>Error</section>
+const titles = {
+  404: '404',
+  500: 'Indisponible'
+}
+
+const messages = {
+  404: "La page que vous recherchez n'existe pas ou a été déplacée...",
+  500: "Malheuresement cette page ou cette fonctionnalité n'est pas encore disponible..."
+}
+
+export const Error = ({ layout, status }) => (
+  <section className={css.error}>
+    <h1 className={css.title}>{titles[status]}</h1>
+    <p className={css.text}>{messages[status]}</p>
+    <Link className={css.link} to={layout === 'app' ? '/app' : '/'}>
+      {layout === 'app' ? 'Tableau de bord' : 'Connectez-vous'}
+    </Link>
+  </section>
+)
--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,3 +1,27 @@
+import { useNavigate } from 'react-router-dom'
 import css from '@/pages/login/login.module.scss'
 
-export const Login = () => <section className={css.login}>Login</section>
+const ids = [12, 18]
+
+export const Login = () => {
+  const navigate = useNavigate()
+
+  const handleLogin = id => localStorage.setItem('userId', id) || navigate('/app')
+
+  return (
+    <section className={css.login}>
+      <h1 className={css.title}>Connexion</h1>
+      <p className={css.alert}>
+        Projet 12 de la formation OpenClassrooms, réalisation d&apos;un tableau de bord de suivi de sportifs. Pour
+        accéder à l&apos;application,veuillez vous connecter avec l&apos;un des identifiants suivants :
+      </p>
+      <div className={css.form}>
+        {ids.map(id => (
+          <button className={css.formButton} key={id} onClick={() => handleLogin(id)}>
+            Utilisateur {id}
+          </button>
+        ))}
+      </div>
+    </section>
+  )
+}
