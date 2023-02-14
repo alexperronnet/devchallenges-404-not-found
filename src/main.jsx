20c8ccbe3e5b9bd95d71c3f1af88120cc3edcd64
alexperronnet <githubid00@unbrk.me>
2023-02-14 18:43:32 +0100

    :lipstick: Create default styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import '@/styles/main.scss'
 
 const container = document.querySelector('#root')
 const root = createRoot(container)
