4e4029ec144830dd5d81787fe7efa24721387cb7
alexperronnet <github.ayfb9@lxprnt.com>
2023-02-14 19:03:09 +0100

    :construction: Create main & app layouts

--- src/layouts/main/footer/footer.jsx
+++ src/layouts/main/footer/footer.jsx
@@ -1,3 +1,15 @@
+import { ReactComponent as IconGithub } from '@/assets/icon-github.svg'
 import css from '@/layouts/main/footer/footer.module.scss'
 
-export const MainFooter = () => <footer className={css.footer}>MainFooter</footer>
+const currentYear = new Date().getFullYear()
+const sourceUrl = 'https://github.com/alexperronnet/openclassrooms-p12-sportsee'
+
+export const MainFooter = () => (
+  <footer className={css.footer}>
+    Copiryght, SportSee {currentYear}
+    <a className={css.source} href={sourceUrl} target="_blank" rel="noreferrer noopener">
+      <IconGithub className={css.sourceIcon} />
+      <span className={css.sourceLabel}>Voir la source</span>
+    </a>
+  </footer>
+)
