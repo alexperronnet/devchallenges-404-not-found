6e55694792a8427fea0b391b8c7fc4a8b95e2dbb
alexperronnet <github.ayfb9@lxprnt.com>
2023-02-18 19:49:52 +0100

    :iphone: Disable dashboard on mobile

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,21 +1,28 @@
+import { useIsMobile } from '@/hooks'
 import { Loader, ErrorMessage, Hero, Analytics } from '@/components'
 import { DataConsumer } from '@/context'
 import css from '@/pages/dashboard/dashboard.module.scss'
 
-export const Dashboard = () => (
-  <DataConsumer>
-    {({ loading, error, data }) => {
-      if (loading) return <Loader />
-      if (error) return <ErrorMessage title="Erreur" message="Impossible de charger les données." />
+export const Dashboard = () => {
+  const { isMobile } = useIsMobile(640)
 
-      const firstName = data.mainData.userInfos.firstName
+  return (
+    <DataConsumer>
+      {({ loading, error, data }) => {
+        if (loading) return <Loader />
+        if (error) return <ErrorMessage title="Erreur" message="Impossible de charger les données." />
 
-      return (
-        <section className={css.dashboard}>
-          <Hero firstName={firstName} />
-          <Analytics data={data} />
-        </section>
-      )
-    }}
-  </DataConsumer>
-)
+        const firstName = data.mainData.userInfos.firstName
+
+        return isMobile ? (
+          <ErrorMessage title="Dashboard indisponible" message="Vos statistiques ne sont pas disponibles sur mobile." />
+        ) : (
+          <section className={css.dashboard}>
+            <Hero firstName={firstName} />
+            <Analytics data={data} />
+          </section>
+        )
+      }}
+    </DataConsumer>
+  )
+}
